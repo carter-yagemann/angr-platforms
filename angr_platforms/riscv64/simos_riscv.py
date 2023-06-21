c58532ce969f7dda9041154f317aaa31392050d2
Carter Yagemann <yagemann.1@osu.edu>
2023-06-21 16:43:38 -0400

    Bugfixes.

--- angr_platforms/riscv64/simos_riscv.py
+++ angr_platforms/riscv64/simos_riscv.py
@@ -1,25 +1,33 @@
 from angr.simos import SimOS, register_simos
 from angr.sim_procedure import SimProcedure
-from angr.calling_conventions import SimStackArg, SimRegArg, SimCC, register_syscall_cc, register_default_cc, SimCC
-from .arch_riscv import ArchRISCV
+from angr.calling_conventions import (
+    SimStackArg,
+    SimRegArg,
+    SimCC,
+    register_syscall_cc,
+    register_default_cc,
+    SimCC,
+)
+from archinfo.arch_riscv64 import ArchRISCV64
 
 
 class SimCCRISCV(SimCC):
-    ARG_REGS = [ 'a0', 'a1', 'a2', 'a3', 'a4', 'a5']
-    FP_ARG_REGS = []    # expand in case the floating point extension is added
+    ARG_REGS = ["a0", "a1", "a2", "a3", "a4", "a5"]
+    FP_ARG_REGS = []  # expand in case the floating point extension is added
     STACK_ALIGNMENT = 16
-    RETURN_ADDR = SimRegArg('ra', 8)
-    RETURN_VAL = SimRegArg('a0', 8)
-    ARCH = ArchRISCV
+    RETURN_ADDR = SimRegArg("ra", 8)
+    RETURN_VAL = SimRegArg("a0", 8)
+    ARCH = ArchRISCV64
+
 
 class SimRISCVSyscall(SimCC):
-    ARG_REGS = ['a0', 'a1', 'a2', 'a3', 'a4', 'a5']
-    RETURN_VAL = SimRegArg('a0', 8)
+    ARG_REGS = ["a0", "a1", "a2", "a3", "a4", "a5"]
+    RETURN_VAL = SimRegArg("a0", 8)
     RETURN_ADDR = SimStackArg(8, 8)
-    ARCH = ArchRISCV
+    ARCH = ArchRISCV64
 
     @staticmethod
-    def _match(arch, args, sp_delta):   # pylint: disable=unused-argument
+    def _match(arch, args, sp_delta):  # pylint: disable=unused-argument
         # doesn't appear anywhere but syscalls
         return False
 
@@ -27,5 +35,6 @@ class SimRISCVSyscall(SimCC):
     def syscall_num(state):
         return state.regs.a7
 
-register_syscall_cc('RISCV64', 'Linux', SimRISCVSyscall)
-register_default_cc('RISCV64', SimCCRISCV)
+
+register_syscall_cc("RISCV64", "Linux", SimRISCVSyscall)
+register_default_cc("RISCV64", SimCCRISCV)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='angr_platforms',
-    version='0.2',
+    version='0.1',
     description='A collection of extra platforms for angr',
     packages=find_packages(),
     install_requires=[
